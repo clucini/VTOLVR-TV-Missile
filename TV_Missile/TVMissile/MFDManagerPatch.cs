f1d1691564a8f5d437163ac1f581c0a054d02cf4
Clucini <Clucini07@gmail.com>
2023-06-10 12:38:03 +1000

    patch

---
+++ TV_Missile/TVMissile/MFDManagerPatch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using Harmony;
+
+[HarmonyPatch(typeof(MFDManager), "SetupDict")]
+class PatchSetupDict
+{
+	public Prefix(MFDManager __instance)
+	{
+		__instance.gameObject.AddComponent<>()
+	}
+}
